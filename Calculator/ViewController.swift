ee6f7805481cc46bcc8cbbd796b9c265244f1098
Iskender <iskendergurun@yahoo.com>
2025-11-03 04:05:24 +0300

    IOS Calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ilksayi: UITextField!
+    @IBOutlet weak var ikincisayi: UITextField!
+    @IBOutlet weak var sonuc: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var myNumber = 1
+        myNumber = 2
     }
 
 
+    @IBAction func toplama(_ sender: Any) {
+        
+        if let firstNumber = Int(ilksayi.text!) {
+            if let secondNumber = Int(ikincisayi.text!) {
+                let result = firstNumber + secondNumber
+                sonuc.text = String(result)
+            }
+        }
+    }
+    
+    
+    @IBAction func cikartma(_ sender: Any) {
+        if let firstNumber = Int(ilksayi.text!) {
+            if let secondNumber = Int(ikincisayi.text!) {
+                let result = firstNumber - secondNumber
+                sonuc.text = String(result)
+            }
+        }
+    }
+    
+    
+    @IBAction func carpma(_ sender: Any) {
+        if let firstNumber = Int(ilksayi.text!) {
+            if let secondNumber = Int(ikincisayi.text!) {
+                let result = firstNumber * secondNumber
+                sonuc.text = String(result)
+            }
+        }
+    }
+    
+    
+    @IBAction func bolme(_ sender: Any) {
+        if let firstNumber = Int(ilksayi.text!) {
+            if let secondNumber = Int(ikincisayi.text!) {
+                let result = firstNumber / secondNumber
+                sonuc.text = String(result)
+            }
+        }
+    }
 }
 
